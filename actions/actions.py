90307e5fdc0dabb0ee713f927cb9f43e65751f2d
Vivi Huang <rwhuang@thoughtworks.com>
2019-09-16 18:49:13 +0800

    Fix config.

--- actions/actions.py
+++ actions/actions.py
@@ -1,150 +1,11 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
 import logging
-import json
 from typing import Text, Dict, Any, List
 
 from rasa_sdk import Action, Tracker
-from rasa_sdk.events import SlotSet, UserUtteranceReverted, ConversationPaused
 from rasa_sdk.executor import CollectingDispatcher
 
 logger = logging.getLogger(__name__)
-
-
-class ActionGreetUser(Action):
-    """Greets the user with/without privacy policy"""
-
-    def name(self):
-        return "action_greet_user"
-
-    def run(self, dispatcher, tracker, domain):
-        intent = tracker.latest_message["intent"].get("name")
-        shown_privacy = tracker.get_slot("shown_privacy")
-        name_entity = next(tracker.get_latest_entity_values("name"), None)
-        if intent == "greet":
-            if shown_privacy and name_entity and name_entity.lower() != "sara":
-                dispatcher.utter_template("utter_greet_name", tracker, name=name_entity)
-                return []
-            elif shown_privacy:
-                dispatcher.utter_template("utter_greet_noname", tracker)
-                return []
-            else:
-                dispatcher.utter_template("utter_greet", tracker)
-                dispatcher.utter_template("utter_inform_privacypolicy", tracker)
-                return [SlotSet("shown_privacy", True)]
-        return []
-
-
-class ActionChitchat(Action):
-    """Returns the chitchat utterance dependent on the intent"""
-
-    def name(self):
-        return "action_chitchat"
-
-    def run(self, dispatcher, tracker, domain):
-        intent = tracker.latest_message["intent"].get("name")
-
-        # retrieve the correct chitchat utterance dependent on the intent
-        if intent in [
-            "ask_weather",
-            "ask_howdoing",
-            "ask_whatspossible",
-            "ask_isbot",
-            "ask_howold",
-            "ask_languagesbot",
-            "ask_time",
-            "ask_wherefrom",
-            "ask_whoami",
-            "handleinsult",
-            "nicetomeeyou",
-            "telljoke",
-            "ask_whatismyname",
-            "ask_whoisit",
-        ]:
-            dispatcher.utter_template("utter_" + intent, tracker)
-        return []
-
-
-class ActionDefaultAskAffirmation(Action):
-    """Asks for an affirmation of the intent if NLU threshold is not met."""
-
-    def name(self) -> Text:
-        return "action_default_ask_affirmation"
-
-    def __init__(self) -> None:
-        import pandas as pd
-
-        self.intent_mappings = pd.read_csv("data/" "intent_description_mapping.csv")
-        self.intent_mappings.fillna("", inplace=True)
-        self.intent_mappings.entities = self.intent_mappings.entities.map(
-            lambda entities: {e.strip() for e in entities.split(",")}
-        )
-
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List["Event"]:
-
-        intent_ranking = tracker.latest_message.get("intent_ranking", [])
-        if len(intent_ranking) > 1:
-            diff_intent_confidence = intent_ranking[0].get(
-                "confidence"
-            ) - intent_ranking[1].get("confidence")
-            if diff_intent_confidence < 0.2:
-                intent_ranking = intent_ranking[:2]
-            else:
-                intent_ranking = intent_ranking[:1]
-        first_intent_names = [
-            intent.get("name", "")
-            for intent in intent_ranking
-            if intent.get("name", "") != "out_of_scope"
-        ]
-
-        message_title = (
-            "Sorry, I'm not sure I've understood " "you correctly 🤔 Do you mean..."
-        )
-
-        entities = tracker.latest_message.get("entities", [])
-        entities = {e["entity"]: e["value"] for e in entities}
-
-        entities_json = json.dumps(entities)
-
-        buttons = []
-        for intent in first_intent_names:
-            logger.debug(intent)
-            logger.debug(entities)
-            buttons.append(
-                {
-                    "title": self.get_button_title(intent, entities),
-                    "payload": "/{}{}".format(intent, entities_json),
-                }
-            )
-
-        buttons.append({"title": "Something else", "payload": "/out_of_scope"})
-
-        dispatcher.utter_button_message(message_title, buttons=buttons)
-
-        return []
-
-    def get_button_title(self, intent: Text, entities: Dict[Text, Text]) -> Text:
-        default_utterance_query = self.intent_mappings.intent == intent
-        utterance_query = (
-            self.intent_mappings.entities == entities.keys() & default_utterance_query
-        )
-
-        utterances = self.intent_mappings[utterance_query].button.tolist()
-
-        if len(utterances) > 0:
-            button_title = utterances[0]
-        else:
-            utterances = self.intent_mappings[default_utterance_query].button.tolist()
-            button_title = utterances[0] if len(utterances) > 0 else intent
-
-        return button_title.format(**entities)
 
 
 class ActionDefaultFallback(Action):
@@ -152,37 +13,10 @@ class ActionDefaultFallback(Action):
         return "action_default_fallback"
 
     def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
     ) -> List["Event"]:
-        # Fallback caused by TwoStageFallbackPolicy
-        if (
-            len(tracker.events) >= 4
-            and tracker.events[-4].get("name") == "action_default_ask_affirmation"
-        ):
-
-            dispatcher.utter_template("utter_restart_with_button", tracker)
-
-            return [SlotSet("feedback_value", "negative"), ConversationPaused()]
-
-        # Fallback caused by Core
-        else:
-            dispatcher.utter_template("utter_default", tracker)
-            return [UserUtteranceReverted()]
-
-
-class ActionNextStep(Action):
-    def name(self):
-        return "action_next_step"
-
-    def run(self, dispatcher, tracker, domain):
-        step = int(tracker.get_slot("step")) + 1
-
-        if step in [2, 3, 4]:
-            dispatcher.utter_template("utter_continue_step{}".format(step), tracker)
-        else:
-            dispatcher.utter_template("utter_no_more_steps", tracker)
-
+        dispatcher.utter_template("utter_default", tracker)
         return []
